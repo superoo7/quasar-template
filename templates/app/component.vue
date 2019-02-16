e9e02a3e31746cfef9e5652687d6ad73ed158b30
Lai Weng Han <jlwhoo7@gmail.com>
2019-02-16 10:28:51 +0800

    initialize project

---
+++ templates/app/component.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>Component</div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extends({
+  // name: 'ComponentName',
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {}
+});
+</script>
+
+<style>
+</style>
---
+++ templates/app/page.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extends({
+  // name: 'ComponentName',
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {}
+});
+</script>
+
+<style>
+</style>
